1befd2074d926f863898de78769aea291914112d
JohnMDoll <johnmdoll3@gmail.com>
2022-11-15 13:53:02 -0600

    finished project up to bonus work

--- src/components/tickets/Ticket.js
+++ src/components/tickets/Ticket.js
@@ -1,58 +1,107 @@
 import { Link } from "react-router-dom"
+import "./Tickets.css"
 
 export const Ticket = ({ ticketObject, getAllTickets, currentUser, employees }) => {
 
     let assignedEmployee = null
 
-    if (ticketObject.employeeTickets.length > 0){
+    if (ticketObject.employeeTickets.length > 0) {
         const ticketEmployeeRelationship = ticketObject.employeeTickets[0]
         assignedEmployee = employees.find(employee => employee.id === ticketEmployeeRelationship.employeeId)
     }
 
     const userEmployee = employees.find(employee => employee.userId === currentUser.id)
-    
-    const buttonOrNoButton = () =>{
-        if (currentUser.staff) {
-            return <button
-            onClick={() => {
-                fetch(`http://localhost:8088/employeeTickets`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                    employeeId: userEmployee.id,
-                    serviceTicketId: ticketObject.id
-                    })
-                })
-                    .then(response => response.json())
-                    .then(() => {
-                        getAllTickets()
-                    })
-            }}
-            >Claim</button>
-        } 
+
+    const canClose = () => {
+        if (currentUser.staff && userEmployee?.id === assignedEmployee?.id && ticketObject.dateCompleted === "") {
+            return <button onClick={closeTicket} className="ticket__finish">Finish</button>
+        }
         else {
             return ""
         }
     }
 
+    const deleteButton = () => {
+        if (!currentUser.staff) {
+            return <button onClick={() => {
+                fetch(`http://localhost:8088/serviceTickets/${ticketObject.id}`, {
+            method: "DELETE"
+        })
+            .then(getAllTickets)
+            }} className="ticket__finish">Delete</button>
+        }
+        else {
+            return ""
+        }
+    }
+
+    const closeTicket = () => {
+        const copy = {
+            userId: ticketObject.userId,
+            description: ticketObject.description,
+            emergency: ticketObject.emergency,
+            dateCompleted: new Date()
+        }
+
+        return fetch(`http://localhost:8088/serviceTickets/${ticketObject.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(copy)
+        })
+            .then(res => res.json())
+            .then(getAllTickets)
+    }
+
+    const buttonOrNoButton = () => {
+        if (currentUser.staff) {
+            return <button
+                onClick={() => {
+                    fetch(`http://localhost:8088/employeeTickets`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            employeeId: userEmployee.id,
+                            serviceTicketId: ticketObject.id
+                        })
+                    })
+                        .then(response => response.json())
+                        .then(() => {
+                            getAllTickets()
+                        })
+                }}
+            >Claim</button>
+        }
+        else {
+            return "Ticket not yet assigned"
+        }
+    }
+
     return <section className="ticket" key={`ticket--${ticketObject.id}`}>
-    <header>
-        {
-            currentUser.staff
-            ? `Ticket ${ticketObject.id}`
-            : <Link to={`/tickets/${ticketObject.id}/edit`}>Ticket {ticketObject.id}</Link>
-        }
-    </header>
-    <section>{ticketObject.description}</section>
-    <section>Emergency: {ticketObject.emergency ? "🧨" : "No"}</section>
-    <footer>
-        {
-        ticketObject.employeeTickets.length
-        ? `Currently being worked on by ${assignedEmployee !== null ? assignedEmployee?.user?.fullName : ""}`
-        : buttonOrNoButton()
-        }
-    </footer>
-</section>
+        <header>
+            {
+                currentUser.staff
+                    ? `Ticket ${ticketObject.id}`
+                    : <Link to={`/tickets/${ticketObject.id}/edit`}>Ticket {ticketObject.id}</Link>
+            }
+        </header>
+        <section>{ticketObject.description}</section>
+        <section>Emergency: {ticketObject.emergency ? "🧨" : "No"}</section>
+        <footer>
+            {
+                ticketObject.employeeTickets.length
+                    ? `Currently being worked on by ${assignedEmployee !== null ? assignedEmployee?.user?.fullName : ""}`
+                    : buttonOrNoButton()
+            }
+            {
+                canClose()
+            }
+            {
+                deleteButton()
+            }
+        </footer>
+    </section>
 }
